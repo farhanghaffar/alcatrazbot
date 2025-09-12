c476d69bec05673723a390e8e081be987a4a31de
farhan <farhan.ghffar@gmail.com>
2025-09-12 19:17:09 +0500

    1- Added/Manage cool time in Machines model

--- api/models/Machine.js
+++ api/models/Machine.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const machineSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  url: { type: String, required: true }
+  url: { type: String, required: true },
+  lastSwitchTime: { type: Date, default: null },
 });
 
-module.exports = mongoose.model('Machine', machineSchema);
+module.exports = mongoose.model("Machine", machineSchema);
